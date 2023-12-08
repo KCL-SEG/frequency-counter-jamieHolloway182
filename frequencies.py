4492dcdad728eacad14d715e3d2423ea7c51e5de
Jamie Holloway <jamie@cadiahq.com>
2023-12-08 11:40:06 +0000

    Solved task

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for i in items:
+        string = str(i)
+        if string in frequencies:
+            frequencies[string] += 1
+        else:
+            frequencies[string] = 1
     return frequencies
